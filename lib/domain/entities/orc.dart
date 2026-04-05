7dd063b807946be43b01744ebf6893ebc57f5722
Eduardo Lovatel <edulova1104@gmail.com>
2026-04-05 01:02:53 -0300

    fix: remove arquivos duplicados

--- lib/domain/entities/orc.dart
+++
@@ -1,9 +0,0 @@
-import 'package:rpg_flutter/entities/raca.dart';
-
-class Orc extends Raca {
-  Orc({
-    required super.bonusVida,
-    required super.bonusEscudo,
-    required super.bonusAtaque,
-  });
-}
